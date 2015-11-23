1a78d799b49232a73c4804fa91fc345a5abb8187
kajiiiro <ghfjdksla67584930@yahoo.co.jp>
2015-11-23 11:36:53 +0900

    add error handring, add iv (do not use yet)

--- routes/index.js
+++ routes/index.js
@@ -4,27 +4,31 @@ var crypto = require('crypto');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var data = req.query.data || 'hello world';
-  crypto.randomBytes(32, function(err, key) {
-    if (err) {
-      console.log(err);
-      return res.status(500).json("error");
-    }
-    var cipher = crypto.createCipheriv('aes-256-ecb', key, '');
-    cipher.update(data, 'utf8');
-    var encrypted = cipher.final();
-    var decipher = crypto.createDecipheriv('aes-256-ecb', key, '');
-    decipher.update(encrypted, 'base64');
-    var decrypted = decipher.final('utf8');
+  try {
+    var data = req.query.data || 'hello world';
+    var B         = 'base64';
+    var U         = 'utf8';
+    var keyBytes  = 32; // 16 or 32
+    var algorithm = 'aes-' + (keyBytes * 8) + '-ecb';
+    var key       = crypto.randomBytes(keyBytes);
+    var iv        = crypto.randomBytes(keyBytes);
+    var cipher    = crypto.createCipheriv(algorithm, key, '');
+    var encrypted = cipher.update(data, U, B) + cipher.final(B);
+    var decipher  = crypto.createDecipheriv(algorithm, key, '');
+    var decrypted = decipher.update(encrypted, B, U) + decipher.final(U);
+    console.log(data, key.toString(B), iv.toString(B), encrypted, decrypted);
     res.render('index', {
       title       : data,
-      key         : key.toString('base64'),
-      keyHex      : key.toString('hex'),
-      encrypted   : encrypted.toString('base64'),
-      encryptedHex: encrypted.toString('hex'),
+      key         : key.toString(B),
+      iv          : iv.toString(B),
+      encrypted   : encrypted,
       decrypted   : decrypted,
     });
-  });
+  } catch (err) {
+    console.log(err);
+    err.status = 500;
+    return next(err);
+  }
 });
 
 module.exports = router;
